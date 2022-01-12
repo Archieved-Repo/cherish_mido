8419151709e94faa429fa36bf8865da9027a9f4a
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-01-12 11:15:17 +0000

    mido: Ignore Neverallows
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,9 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# Neverallows
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 BOARD_VNDK_VERSION := current
